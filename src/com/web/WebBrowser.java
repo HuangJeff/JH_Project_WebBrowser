751ba0c2a90c5aafa4222f98475a5796bcc4859c
huangjeff <https://github.com/huangjeff>
2015-09-30 22:27:10 +0800

    建立大剛

---
+++ src/com/web/WebBrowser.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.web;
+
+/**
+ * WebBrowser 主程式<br>
+ * Extends JFrame and implements HyperlinkListener,ActionListener
+ * @author jeff
+ * @date 2015/09/30
+ */
+public class WebBrowser {
+	//建立工具欄來顯示位址欄
+	
+	//建立功能表欄
+	
+	//建立工具欄
+	
+	
+	/**
+	 * Constructor<br>
+	 * 1.設定title=網頁瀏灠器<br>
+	 * 2.各式事件監聽器<br>
+	 * 3.按鈕快捷鍵<br>
+	 * 4.設置大小<br>
+	 * 5.工具欄中新增按鈕元件<br>
+	 */
+	public WebBrowser() {
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
