e6822452732fca9cefbe37e09daac63356be2c62
huangjeff <https://github.com/huangjeff>
2015-12-03 22:39:30 +0800

    message

--- bin/batWebBrowser.bat
+++ bin/batWebBrowser.bat
@@ -1,7 +1,12 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jre1.8.0_45
+set JAVA_HOME=C:\Program Files\Java\jre1.8.0_66
 set PATH=%JAVA_HOME%\bin
+
+rem echo JAVA_HOME : %JAVA_HOME%
+rem echo PATH : %PATH%
+
+rem java -version
 
 java -cp WebBrowser.jar com.web.WebBrowser
 
